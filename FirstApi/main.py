745ee1f8b3f931e6c8554649b7222bb41594a99b
Muhammad Huzaifa <k224337@nu.edu.pk>
2023-07-26 22:35:33 +0500

    Update main.py

--- FirstApi/main.py
+++ FirstApi/main.py
@@ -1,7 +1,10 @@
+from typing import Optional
+
 from fastapi import FastAPI, Depends
 import Models
 from db import engine, SessionLocal
 from sqlalchemy.orm import session
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -18,11 +21,64 @@ def get_db():
             db.close()
 
 
+class Todo(BaseModel):
+    title: str
+    Description: Optional[str]
+    priority: int = Field(gt=0, lt=6, Description="Priority Must be bw 1 and 5")
+    complete: bool
+
 
 @app.get('/')
 async def get_all_data(db: session = Depends(get_db)):
     return db.query(Models.Todos).all()
+
+
 @app.get('/searchbyid/{id}')
 async def get_all_data(id: str, db: session = Depends(get_db)):
-    search_str =  db.query(Models.Todos).filter(Models.Todos.id == id).first()
+    search_str = db.query(Models.Todos).filter(Models.Todos.id == id).first()
     return search_str
+
+
+@app.post('/')
+async def create_todo(todo: Todo, db: session = Depends(get_db)):
+    todo_model = Models.Todos()
+    todo_model.title = todo.title
+    todo_model.Description = todo.Description
+    todo_model.priority = todo.priority
+    todo_model.Complete = todo.complete
+
+    db.add(todo_model)
+    db.commit()
+    return {
+        'status': 201,
+        'transaction': 'SuccesFull'
+    }
+
+
+@app.post('/{todo_id}')
+async def update_todo(todo_id: int, todo: Todo, db: session = Depends(get_db)):
+    todo_model = db.query(Models.Todos).filter(Models.Todos.id == todo_id).first()
+    # return {
+    #     'status': todo_id,
+    #     'transaction': todo_model
+    # }
+    todo_model.title = todo.title
+    todo_model.Description = todo.Description
+    todo_model.priority = todo.priority
+    todo_model.Complete = todo.complete
+    db.add(todo_model)
+    db.commit()
+    return {
+        'status': 201,
+        'transaction': 'SuccesFull'
+    }
+
+
+@app.post('/delete/{todo_id}')
+async def delete_todo(todo_id: int, db: session = Depends(get_db)):
+    db.query(Models.Todos).filter(Models.Todos.id == todo_id).delete()
+    db.commit()
+    return {
+        'status': 201,
+        'transaction': 'SuccesFully Deleted'
+    }
