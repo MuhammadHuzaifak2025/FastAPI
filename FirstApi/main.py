f07c0d0082ba4cf2f0f7281caefc69044d77b046
Muhammad Huzaifa <k224337@nu.edu.pk>
2023-07-30 21:00:19 +0500

    Update main.py

--- FirstApi/main.py
+++ FirstApi/main.py
@@ -21,11 +21,15 @@ def get_db():
             db.close()
 
 
-class Todo(BaseModel):
+class Todos(BaseModel):
     title: str
     Description: Optional[str]
     priority: int = Field(gt=0, lt=6, Description="Priority Must be bw 1 and 5")
     complete: bool
+
+
+class Todolist(BaseModel):
+    todo_list: list[Todos]
 
 
 @app.get('/')
@@ -40,7 +44,7 @@ async def get_all_data(id: str, db: session = Depends(get_db)):
 
 
 @app.post('/')
-async def create_todo(todo: Todo, db: session = Depends(get_db)):
+async def create_todo(todo: Todos, db: session = Depends(get_db)):
     todo_model = Models.Todos()
     todo_model.title = todo.title
     todo_model.Description = todo.Description
@@ -56,7 +60,7 @@ async def create_todo(todo: Todo, db: session = Depends(get_db)):
 
 
 @app.post('/{todo_id}')
-async def update_todo(todo_id: int, todo: Todo, db: session = Depends(get_db)):
+async def update_todo(todo_id: int, todo: Todos, db: session = Depends(get_db)):
     todo_model = db.query(Models.Todos).filter(Models.Todos.id == todo_id).first()
     # return {
     #     'status': todo_id,
@@ -82,3 +86,18 @@ async def delete_todo(todo_id: int, db: session = Depends(get_db)):
         'status': 201,
         'transaction': 'SuccesFully Deleted'
     }
+
+
+@app.post('/Create_Todos')
+async def create_todos(todos: Todolist, db: session = Depends(get_db)):
+    models = []
+    for e in todos.todo_list:
+        todo_model = Models.Todos()
+        todo_model.title = e.title
+        todo_model.Description = e.Description
+        todo_model.priority = e.priority
+        todo_model.Complete = e.complete
+        models.append(todo_model)
+    db.add(models)
+    db.commit()
+    return "true"
