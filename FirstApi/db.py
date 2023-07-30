cb75362dbc87cbca20e6f5d0c1355f78aa16a89a
Muhammad Huzaifa <k224337@nu.edu.pk>
2023-07-30 21:00:33 +0500

    Update db.py

--- FirstApi/db.py
+++ FirstApi/db.py
@@ -3,12 +3,13 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQL_ALCHEMY_DATABASE_ALCHEMY = "sqlite:///./todos.db"
+# SQL_ALCHEMY_DATABASE_ALCHEMY = "sqlite:///./todos.db"
+SQL_ALCHEMY_DATABASE_ALCHEMY = "mysql+pymysql://root@localhost:3306/test"
 
 engine = create_engine(
-    SQL_ALCHEMY_DATABASE_ALCHEMY,connect_args= {"check_same_thread" : False}
+    SQL_ALCHEMY_DATABASE_ALCHEMY
+    # SQL_ALCHEMY_DATABASE_ALCHEMY,connect_args= {"check_same_thread" : False}
 )
-SessionLocal = sessionmaker(autocommit = False, autoflush= False, bind=engine)
-# seccionLocal = sessionmaker(autoflush= False, autocommit = False, bind=)
-Base = declarative_base()
 
+SessionLocal = sessionmaker(autocommit = False, autoflush= False, bind=engine)
+Base = declarative_base()
